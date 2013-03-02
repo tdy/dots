32978067aad2b0a3c864244c307c240bfb06fccd
Tim Yang <timothy.d.yang@gmail.com>
2013-03-01 23:06:07 -0600

    reformat
    Former-commit-id: 4f9a6c6ee261c652bdef85cb71dcc7e61476d1d4

--- .config/awesome/themes/dust/theme.lua
+++ .config/awesome/themes/dust/theme.lua
@@ -7,95 +7,107 @@
 local awful = require("awful")
 
 -- {{{ Helpers
-function hometheme(path)
-  return awful.util.getdir("config") .. "/themes/dust" .. path
-end
+function hometheme(path) return awful.util.getdir("config") .. "/themes/dust" .. path end
 function hometags(path) return hometheme("/taglist") .. path end
 function homelayouts(path) return hometheme("/layouts") .. path end
 function homewidgets(path) return hometheme("/widgets") .. path end
 function hometp(path) return hometheme("/tp") .. path end
 
-function systheme(path)
-  return "/usr/share/awesome/themes/default" .. path
-end
+function systheme(path) return "/usr/share/awesome/themes/default" .. path end
 function systitle(path) return systheme("/titlebar") .. path end
 -- }}}
 
--- {{{ Main
+-- {{ Main
 theme = {}
-theme.wallpaper = hometheme("/background.jpg")
-theme.font      = "ubuntu mono 18"
+theme.wallpaper             = hometheme("/background.jpg")
+theme.font                  = "ubuntu mono 18"
+theme.menu_height           = 15
+theme.menu_width            = 100
 -- }}}
 
 -- {{{ Colors
-theme.fg_normal        = "#aaaaaa"
-theme.fg_focus         = "#111111"
-theme.fg_urgent        = "#ffffff"
-theme.fg_tooltip       = "#1a1a1a"
-theme.fg_em            = "#d6d6d6"
+theme.fg_normal             = "#aaaaaa"
+theme.fg_focus              = "#111111"
+theme.fg_urgent             = "#ffffff"
+theme.fg_tooltip            = "#1a1a1a"
+theme.fg_em                 = "#d6d6d6"
+theme.fg_widget             = theme.bg_focus
+theme.fg_center_widget      = "#636363"
+theme.fg_end_widget         = theme.fg_tooltip
 
-theme.bg_normal        = theme.fg_tooltip
-theme.bg_focus         = "#908884"
-theme.bg_urgent        = "#cd7171"
-theme.bg_tooltip       = theme.fg_em
-theme.bg_em            = "#5a5a5a"
-theme.bg_systray       = theme.fg_tooltip
+theme.bg_normal             = theme.fg_tooltip
+theme.bg_focus              = "#908884"
+theme.bg_urgent             = "#cd7171"
+theme.bg_tooltip            = theme.fg_em
+theme.bg_em                 = "#5a5a5a"
+theme.bg_systray            = theme.fg_tooltip
+theme.bg_widget             = "#2a2a2a"
 
-theme.fg_widget        = theme.bg_focus
-theme.fg_center_widget = "#636363"
-theme.fg_end_widget    = theme.fg_tooltip
-theme.bg_widget        = "#2a2a2a"
+theme.border_width          = 1
+theme.border_normal         = "#222222"
+theme.border_focus          = theme.bg_focus
+theme.border_marked         = "#91231c"
+theme.border_tooltip        = "#444444"
+theme.border_widget         = "#3f3f3f"
+
+theme.titlebar_bg_focus     = theme.border_widget
+theme.titlebar_bg_normal    = theme.border_widget
+
+theme.mouse_finder_color    = "#cc9393"
 -- }}}
 
--- {{{ Borders
-theme.border_width   = 1
-theme.border_normal  = "#222222"
-theme.border_focus   = theme.bg_focus
-theme.border_marked  = "#91231c"
-theme.border_tooltip = "#444444"
-theme.border_widget  = "#3f3f3f"
--- }}}
-
--- {{{ Titlebars
-theme.titlebar_bg_focus  = theme.border_widget
-theme.titlebar_bg_normal = theme.border_widget
--- }}}
-
--- {{{ Mouse finder
-theme.mouse_finder_color = "#cc9393"
--- }}}
-
--- {{{ Menu
-theme.menu_height = 15
-theme.menu_width  = 100
--- }}}
-
--- {{{ Taglist
+-- {{{ Icons
 theme.taglist_squares_sel   = hometags("/squaref.png")
 theme.taglist_squares_unsel = hometags("/square.png")
--- }}}
 
--- {{{ Misc
-theme.awesome_icon      = hometheme("/awesome-dust22.png")
-theme.menu_submenu_icon = systheme("/submenu.png")
--- }}}
+theme.awesome_icon          = hometheme("/awesome-dust22.png")
+theme.menu_submenu_icon     = systheme("/submenu.png")
 
--- {{{ Layout
-theme.layout_tile       = homelayouts("/tilew.png")
-theme.layout_tileleft   = homelayouts("/tileleftw.png")
-theme.layout_tilebottom = homelayouts("/tilebottomw.png")
-theme.layout_tiletop    = homelayouts("/tiletopw.png")
-theme.layout_fairv      = homelayouts("/fairvw.png")
-theme.layout_fairh      = homelayouts("/fairhw.png")
-theme.layout_spiral     = homelayouts("/spiralw.png")
-theme.layout_dwindle    = homelayouts("/dwindlew.png")
-theme.layout_max        = homelayouts("/maxw.png")
-theme.layout_fullscreen = homelayouts("/fullscreenw.png")
-theme.layout_magnifier  = homelayouts("/magnifierw.png")
-theme.layout_floating   = homelayouts("/floatingw.png")
--- }}}
+theme.layout_tile           = homelayouts("/tilew.png")
+theme.layout_tileleft       = homelayouts("/tileleftw.png")
+theme.layout_tilebottom     = homelayouts("/tilebottomw.png")
+theme.layout_tiletop        = homelayouts("/tiletopw.png")
+theme.layout_fairv          = homelayouts("/fairvw.png")
+theme.layout_fairh          = homelayouts("/fairhw.png")
+theme.layout_spiral         = homelayouts("/spiralw.png")
+theme.layout_dwindle        = homelayouts("/dwindlew.png")
+theme.layout_max            = homelayouts("/maxw.png")
+theme.layout_fullscreen     = homelayouts("/fullscreenw.png")
+theme.layout_magnifier      = homelayouts("/magnifierw.png")
+theme.layout_floating       = homelayouts("/floatingw.png")
 
--- {{{ Titlebar
+theme.widget_disk           = homewidgets("/disk.png")
+theme.widget_cpu            = homewidgets("/cpu.png")
+theme.widget_ac             = homewidgets("/ac.png")
+theme.widget_acblink        = homewidgets("/acblink.png")
+theme.widget_blank          = homewidgets("/blank.png")
+theme.widget_batfull        = homewidgets("/batfull.png")
+theme.widget_batmed         = homewidgets("/batmed.png")
+theme.widget_batlow         = homewidgets("/batlow.png")
+theme.widget_batempty       = homewidgets("/batempty.png")
+theme.widget_vol            = homewidgets("/vol.png")
+theme.widget_mute           = homewidgets("/mute.png")
+theme.widget_pac            = homewidgets("/pac.png")
+theme.widget_pacnew         = homewidgets("/pacnew.png")
+theme.widget_mail           = homewidgets("/mail.png")
+theme.widget_mailnew        = homewidgets("/mailnew.png")
+theme.widget_temp           = homewidgets("/temp.png")
+theme.widget_tempwarn       = homewidgets("/tempwarm.png")
+theme.widget_temphot        = homewidgets("/temphot.png")
+theme.widget_wifi           = homewidgets("/wifi.png")
+theme.widget_nowifi         = homewidgets("/nowifi.png")
+theme.widget_mpd            = homewidgets("/mpd.png")
+theme.widget_play           = homewidgets("/play.png")
+theme.widget_pause          = homewidgets("/pause.png")
+theme.widget_ram            = homewidgets("/ram.png")
+
+theme.widget_mem            = hometp("/ram.png")
+theme.widget_swap           = hometp("/swap.png")
+theme.widget_fs             = hometp("/fs_01.png")
+theme.widget_fs2            = hometp("/fs_02.png")
+theme.widget_up             = hometp("/up.png")
+theme.widget_down           = hometp("/down.png")
+
 theme.titlebar_close_button_focus               = systitle("/close_focus.png")
 theme.titlebar_close_button_normal              = systitle("/close_normal.png")
 
@@ -118,40 +130,5 @@ theme.titlebar_maximized_button_focus_active    = systitle("/maximized_focus_act
 theme.titlebar_maximized_button_normal_active   = systitle("/maximized_normal_active.png")
 theme.titlebar_maximized_button_focus_inactive  = systitle("/maximized_focus_inactive.png")
 theme.titlebar_maximized_button_normal_inactive = systitle("/maximized_normal_inactive.png")
--- }}}
-
--- {{{ Widgets
-theme.widget_disk     = homewidgets("/disk.png")
-theme.widget_cpu      = homewidgets("/cpu.png")
-theme.widget_ac       = homewidgets("/ac.png")
-theme.widget_acblink  = homewidgets("/acblink.png")
-theme.widget_blank    = homewidgets("/blank.png")
-theme.widget_batfull  = homewidgets("/batfull.png")
-theme.widget_batmed   = homewidgets("/batmed.png")
-theme.widget_batlow   = homewidgets("/batlow.png")
-theme.widget_batempty = homewidgets("/batempty.png")
-theme.widget_vol      = homewidgets("/vol.png")
-theme.widget_mute     = homewidgets("/mute.png")
-theme.widget_pac      = homewidgets("/pac.png")
-theme.widget_pacnew   = homewidgets("/pacnew.png")
-theme.widget_mail     = homewidgets("/mail.png")
-theme.widget_mailnew  = homewidgets("/mailnew.png")
-theme.widget_temp     = homewidgets("/temp.png")
-theme.widget_tempwarn = homewidgets("/tempwarm.png")
-theme.widget_temphot  = homewidgets("/temphot.png")
-theme.widget_wifi     = homewidgets("/wifi.png")
-theme.widget_nowifi   = homewidgets("/nowifi.png")
-theme.widget_mpd      = homewidgets("/mpd.png")
-theme.widget_play     = homewidgets("/play.png")
-theme.widget_pause    = homewidgets("/pause.png")
-theme.widget_ram      = homewidgets("/ram.png")
-
-theme.widget_mem      = hometp("/ram.png")
-theme.widget_swap     = hometp("/swap.png")
-theme.widget_fs       = hometp("/fs_01.png")
-theme.widget_fs2      = hometp("/fs_02.png")
-theme.widget_up       = hometp("/up.png")
-theme.widget_down     = hometp("/down.png")
--- }}}
 
 return theme
